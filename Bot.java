1b14304515d5529b4724589c216b44d6529c56ac
aleksandarchomakov820 <63287130+aleksandarchomakov820@users.noreply.github.com>
2020-06-21 18:05:59 +0200

    Delete Bot.java

--- Bot.java
+++
@@ -1,9 +0,0 @@
-public class Bot {
-
-    public void printMessage() {
-        System.out.println("Bot: You cannot say cat!");
-    }
-    public void removeUser(User user) {
-        ChatRoom.RemoveUser(user);
-    }
-}
